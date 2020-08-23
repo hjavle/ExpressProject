7b6a34920a6a10f4372ed75ab78f440fa3504bfc
Harsha Javle <hjavle@hotmail.com>
2020-08-23 01:15:02 -0400

    created route inside root

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const express = require ('express');
 
 const app = express();
 
-//Routs
+//Routs 
+//created rout to with get method that sends message
 app.get('/', (req,res)=> {
     res.send("We are on home port");
 });
+//Routs 2
+//created another rout inside root with get method that sends message
+app.get('/post', (req,res)=> {
+    res.send("We are on post");
+});
+
 //Starting express listener
 app.listen(3000);
