f603521b0ab8c7f3ce038fb60e1a10aa9b72b0c4
Harsha Javle <hjavle@hotmail.com>
2020-09-02 01:47:36 -0400

    express validation code added

--- index.js
+++ index.js
@@ -1,41 +1,42 @@
-const Joi = require('joi');
 const express = require('express');
 const app = express();
 
 app.use(express.json());
 
-const courses = [
-    {id: 1, name: 'course1'},
-    {id: 2, name: 'course2'},
-    {id: 3, name: 'course3'}
+const names = [
+    {fPIndex: 1, name: 'Harsha'},
+    {fPIndex: 1, name: 'Jane'},
+    {fPIndex: 1, name: 'Jill'}
 ];
 
 app.get('/',(req,res)=> {
-    res.send('Hello World');
+    res.send('Welcome to C02 Calculator Page');
 });
-app.get('/api/courses',(req,res) =>{
+app.get('/api/name',(req,res) =>{
     res.send([1,2,3]);
 });
-app.post('/api/courses', (req,res)=>{
+app.post('/api/names', (req,res)=>{
     if (!req.body.name || req.body.name.length < 3){
         // 404 Bad request
-        res.status(400).send('Name is required and should be minimum 3 characters long!');
+        res.status(400).send("name is required and should be min 3 char long");
         return;
     }
-    const course ={
-        id: course.length + 1,
-        name: req.cody.name
+    const name ={
+        id: name.length + 1,
+        name: req.body.name
     };
-    courses.push(course);
+    names.push(name);
     res.send(course);
 });
 // /api/courses/course 1
-app.get('/api/courses/:id', (req,res)=>{
-const course = courses.find(c => c.id === parseInt(req.params.id));
+app.get('/api/names/:fPIndex', (req,res)=>{
+const name = names.find(c => c.fPIndex === parseInt(req.params.fPIndex));
 if (!course) res.status(404).send('The course with the given ID was not found')
 res.send(course);
 });
+app.put('/api/names/fPIndex', (rer,res)=>{
 
+})
 // Ports
 const port = process.env.PORT || 3000;
 app.listen(port, ()=> console.log(`Listening on port ${port}...`));
