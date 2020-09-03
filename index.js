be0429837cbf0183c6728ac1fc8e44ee7c8965d6
Harsha Javle <hjavle@hotmail.com>
2020-09-02 22:51:49 -0400

    Fixed the APIS

--- index.js
+++ index.js
@@ -3,17 +3,13 @@ const app = express();
 
 app.use(express.json());
 
-const names = [
-    {fPIndex: 1, name: 'Harsha'},
-    {fPIndex: 1, name: 'Jane'},
-    {fPIndex: 1, name: 'Jill'}
-];
+const names = [];
 
 app.get('/',(req,res)=> {
     res.send('Welcome to C02 Calculator Page');
 });
-app.get('/api/name',(req,res) =>{
-    res.send([1,2,3]);
+app.get('/api/names',(req,res) =>{
+    res.send(names);
 });
 app.post('/api/names', (req,res)=>{
     if (!req.body.name || req.body.name.length < 3){
@@ -22,17 +18,18 @@ app.post('/api/names', (req,res)=>{
         return;
     }
     const name ={
-        id: name.length + 1,
+        id: names.length + 1,
         name: req.body.name
     };
     names.push(name);
-    res.send(course);
+    console.log(names);
+    res.send(name);
 });
 // /api/courses/course 1
-app.get('/api/names/:fPIndex', (req,res)=>{
-const name = names.find(c => c.fPIndex === parseInt(req.params.fPIndex));
-if (!name) res.status(404).send('The course with the given ID was not found')
-res.send(course);
+app.get('/api/names/:id', (req,res)=>{
+const name = names.find(c => c.id === parseInt(req.params.id));
+if (!name) res.status(404).send('The name with the given ID was not found')
+res.send(name);
 });
 app.put('/api/names/fPIndex', (rer,res)=>{
 
