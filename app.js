e6c071b89dedccc0428800ce89293fe76e08e126
Harsha Javle <hjavle@hotmail.com>
2020-09-04 01:14:09 -0400

    Fixed Calculation page POST

--- app.js
+++ app.js
@@ -6,32 +6,37 @@ app.use(express.static('.'));
 
 const names = [];
 
-app.get('/api/names',(req,res) =>{
+function getRank()
+{
+    return Math.random();
+}
+
+app.get('/api/names',(req,res) => {
     res.send(names);
 });
-app.post('/api/names', (req,res)=>{
+app.post('/api/names', (req,res)=> {
     if (!req.body.name || req.body.name.length < 3){
         // 404 Bad request
         res.status(400).send("name is required and should be min 3 char long");
         return;
     }
-    const name ={
+
+    const userName = {
         id: names.length + 1,
-        name: req.body.name
+        name: req.body.name,
+        phoneNumber: req.body.phoneNumber,
+        rank: getRank()
     };
-    names.push(name);
+    names.push(userName);
     console.log(names);
-    res.send(name);
+    res.send(userName);
 });
 // /api/courses/course 1
-app.get('/api/names/:id', (req,res)=>{
-const name = names.find(c => c.id === parseInt(req.params.id));
-if (!name) res.status(404).send('The name with the given ID was not found')
-res.send(name);
+app.get('/api/names/:id', (req,res)=> {
+    const name = names.find(c => c.id === parseInt(req.params.id));
+    if (!name) res.status(404).send('The name with the given ID was not found')
+    res.send(name);
 });
-app.put('/api/names/fPIndex', (rer,res)=>{
-
-})
 // Ports
 const port = process.env.PORT || 3000;
 app.listen(port, ()=> console.log(`Listening on port ${port}...`));
