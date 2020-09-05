5b9876ee67d1a94c498e87f8236154625ea1dbbe
Harsha Javle <hjavle@hotmail.com>
2020-09-05 01:35:18 -0400

    fixed radio button names and values

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ app.use(express.static(__dirname));
 
 const names = [];
 
-function getRank(electric,gas,oil,milage,flf,fmf)
+function getRank(electric,gas,oil,milage,flf,fmf,rnp,rc)
 {
-    return((electric * 105) + (gas * 105) + (oil * 113) + (milage * 0.79) + (flf * 1100) + (fmf * 4400));
+    let sum = ((electric * 105) + (gas * 105) + (oil * 113) + (milage * 0.79) + (flf * 1100) + (fmf * 4400));
+    if (!rnp && !rc) sum += 150;
+    return sum;
 }
 
 app.get('/api/names',(req,res) => {
@@ -27,6 +29,8 @@ app.post('/api/names', (req,res)=> {
     let m = req.body.milage;
     let flf = req.body.lessThan4HourFlights;
     let fmf = req.body.moreThan4HourFlights;
+    let rnp = req.body.newspaperRecycle;
+    let rc =  req.body.recycleCans;
 
     const userName = {
         id: names.length + 1,
@@ -37,14 +41,16 @@ app.post('/api/names', (req,res)=> {
         gas: g,
         oil: o,
         milage: m,
-        rank: getRank(e,g,o,m,flf,fmf)
+        newspaperRecycle : rnp,
+        recycleCans : rc,
+        rank: getRank(e,g,o,m,flf,fmf,rnp,rc)
     };
 
     names.push(userName);
     console.log(userName);
     res.send(userName);
 });
-// /api/courses/course 1
+// /api/names/name1
 app.get('/api/names/:id', (req,res)=> {
     const name = names.find(c => c.id === parseInt(req.params.id));
     if (!name) res.status(404).send('The name with the given ID was not found')
