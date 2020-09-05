0bdec1465153296dfbade9d38de50a7a77ccbaf2
Harsha Javle <hjavle@hotmail.com>
2020-09-04 20:56:30 -0400

    corrected base director path

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
-app.use(express.static('__dirname'));
+app.use(express.static(__dirname));
 
 const names = [];
 
